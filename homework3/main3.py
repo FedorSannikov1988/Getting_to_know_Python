9c5a642c5a0c3e9a8f81ad9ca8fa8db61fdf4010
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2022-11-29 01:54:20 +0300

    fixed bugs homework namber 3

--- homework3/main3.py
+++ homework3/main3.py
@@ -8,25 +8,45 @@
 2 -> 10
 '''
 
-def conversion_decimal_binary(natural_number: int) -> str:
+import copy
+
+def conversion_decimal_binary(natural_number: int) -> str|int:
 
     string_resalt = str()
+    
+    copi_natural_number_one = copy.deepcopy(natural_number)
+    copi_natural_number_two = copy.deepcopy(natural_number)
 
     if natural_number < 0:
-        natural_number = abs(natural_number)
+        copi_natural_number_one = abs(copi_natural_number_one)
+        copi_natural_number_two = abs(copi_natural_number_two)
         simvol = "-0b"
+        flag_znak = -1
     else:
-        simvol = "0b"    
+        simvol = "0b"
+        flag_znak = 1
+
+    count_digit_nambers = 0
     
-    while natural_number > 0:
-        string_resalt = str(natural_number%2) + string_resalt
-        natural_number = natural_number//2
+    while copi_natural_number_one > 0:
+        copi_natural_number_one = copi_natural_number_one//2
+        count_digit_nambers += 1
+        
+    int_resalt = 0
     
-    return(simvol + string_resalt)
+    for count in range(0, count_digit_nambers, 1):
+        int_resalt += ((copi_natural_number_two%2)*(10**count))
+        string_resalt = (str(copi_natural_number_two%2) + string_resalt)
+        copi_natural_number_two = (copi_natural_number_two//2)
+
+    #tuple_resalt = tuple([simvol+string_resalt, flag_znak*int_resalt])
+    
+    return (simvol+string_resalt, flag_znak*int_resalt)
 
 natural_number = int(input("Введите натуральное число для преобразования из 10-ой формы записи в 2-ую: "))
 
 print(f"Спомощью встроенной функции способ №1: {bin(natural_number)}")
 print(f"Спомощью встроенной функции способ №2: {format(natural_number, '#b')}")
 print(f"Спомощью встроенной функции способ №3: {format(natural_number, 'b')}")
-print(f"Спомощью моей функции способ №4: {conversion_decimal_binary(natural_number)}")
+print(f"Спомощью моей функции способ №4: {conversion_decimal_binary(natural_number)[0]}")
+print(f"Спомощью моей функции способ №5: {conversion_decimal_binary(natural_number)[1]}")
--- homework3/main4.py
+++ homework3/main4.py
@@ -7,7 +7,8 @@
 [-21 ,13, -8, 5, -3, 2, -1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 '''
 
-def namber_positive_fibonacci(namber: int) -> int:
+def namber_positive_fibonacci(namber: int) -> int:  
+
     if namber == 0:
         return 0
     elif namber in [1, 2]:
@@ -15,48 +16,30 @@ def namber_positive_fibonacci(namber: int) -> int:
     else:
         return (namber_positive_fibonacci(namber-1) + namber_positive_fibonacci(namber-2))
 
-def create_list_positive_fibonacci(lenght_fibonacci: int) -> list:
-
-    lenght_fibonacci = abs(lenght_fibonacci)
-
-    resalt_list = list()
-    
-    for count in range(0, lenght_fibonacci + 1, 1):
-        resalt_list.append(namber_positive_fibonacci(count))
-    
-    return resalt_list
-
-def create_list_negative_fibonacci(list_positive_fibonacci: list) -> list:
-   
-    resalt_list = list()
-    
-    for count in range(0, len(list_positive_fibonacci)-1, 1):
-        
-        if count%2:
-            flag_znak = 1
-        else:
-            flag_znak = -1
-        
-        resalt_list.insert(count,(flag_znak*list_positive_fibonacci[len(list_positive_fibonacci)-count-1]))
-
-    return resalt_list
-
 def create_list_fibonacci(lenght_fibonacci: int) -> list:
-   
-    list_fibonacci = list()
-    
-    list_positive_fibonacci = create_list_positive_fibonacci(lenght_fibonacci)
-    
-    #list_fibonacci = create_list_negative_fibonacci(list_positive_fibonacci) + list_positive_fibonacci
-    list_fibonacci = [*create_list_negative_fibonacci(list_positive_fibonacci), *list_positive_fibonacci]
 
-    return list_fibonacci
+        lenght_fibonacci = abs(lenght_fibonacci)
+        
+        resalt_negative_list = list()
+        
+        resalt_positive_list = list()
+        
+        for count in range(-lenght_fibonacci, 0, 1):
+            resalt_negative_list.append(((-1)**(abs(count)+1))*namber_positive_fibonacci(abs(count)))
+        
+        for count in range(0, lenght_fibonacci + 1, 1):
+            resalt_positive_list.append(namber_positive_fibonacci(count))
+        
+        list_fibonacci = [*resalt_negative_list, *resalt_positive_list]
+                
+        return list_fibonacci
 
 natural_number = int(input("Введите k для которого будет составлен список чисел Фибоначчи в том числе для отрицательных индексов: "))
 
 list_fibonacci = create_list_fibonacci(natural_number)
-
+print("Последовательность Фиббаначи:")
 print(list_fibonacci)
 
 with open('file_create_main4.txt', 'a') as data:
+    data.write(str("Fibonacci:\n"))
     data.write(str(f"{list_fibonacci}\n"))
