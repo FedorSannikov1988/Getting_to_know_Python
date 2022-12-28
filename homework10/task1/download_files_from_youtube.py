f169b0aebdff96461168b0a967452ac3bc96b1db
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2022-12-28 21:49:42 +0300

    stop homework namber 10 + fixed another onesmall bugs

--- homework10/task1/download_files_from_youtube.py
+++ homework10/task1/download_files_from_youtube.py
@@ -15,21 +15,21 @@ def downloading_from_youtube(youtube_video_url: str, \
             use_oauth=flag_for_authentication, \
                 allow_oauth_cache=flag_for_authentication_once)
         
-        for_downloading_video = yt_object.streams.filter(progressive=True).desc().first()
-        
-        for_downloading_audio = yt_object.streams.filter(only_audio=True).desc().first()
-        
-        name_for_file_video = name_for_download_file + "." + \
-            str(str(for_downloading_video).replace('"', '').split("video/")[1].split(" ")[0])
-        
-        name_for_file_audio = name_for_download_file + "_audio" + "." + \
-            str(str(for_downloading_audio).replace('"', '').split("audio/")[1].split(" ")[0])
-        
         if flag_for_download_video:
+            for_downloading_video = yt_object.streams.filter(progressive=True).desc().first()
+
+            name_for_file_video = name_for_download_file + "." + \
+            str(str(for_downloading_video).replace('"', '').split("video/")[1].split(" ")[0])
+
             for_downloading_video.download(download_folder, filename = name_for_file_video)
             print('Downloaded Successfully Video')
             
         if flag_for_download_audio:
+            for_downloading_audio = yt_object.streams.filter(only_audio=True).desc().first()
+
+            name_for_file_audio = name_for_download_file + "_audio" + "." + \
+            str(str(for_downloading_audio).replace('"', '').split("audio/")[1].split(" ")[0])
+
             for_downloading_audio.download(download_folder, filename = name_for_file_audio)
             print('Downloaded Successfully Audio')
     
