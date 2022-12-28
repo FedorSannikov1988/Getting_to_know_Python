2cb08298a516ce2812a6c2d5f7b8f5d0dfcc4688
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2022-12-28 21:09:57 +0300

    stop homework namber 10

--- homework10/task1/graphical_interface.py
+++ homework10/task1/graphical_interface.py
@@ -4,15 +4,34 @@ import download_files_from_youtube as d_f_f_y
 def start_graphical_interface():
     
     windows = tk.Tk()
-    
+
+    download_video_value = tk.IntVar()
+    download_audio_value = tk.IntVar()
+    authentication_value = tk.IntVar()
+    authentication_once_value = tk.IntVar()
+
+    def show_value_checkbutton():
+        print("download_video_value", download_video_value.get())
+        print("download_audio_value", download_audio_value.get())
+        print("authentication_value", authentication_value.get())
+        print("authentication_once_value", authentication_once_value.get())
+        
     def get_url_and_name_download_file():
+        show_value_checkbutton()
         url_txt = youtube_video_url.get("1.0",'end-1c')
         name_txt = name_for_download_file.get("1.0",'end-1c')
         if url_txt:
-            d_f_f_y.downloading_from_youtube(str(url_txt), str(name_txt), True, True, True, True)
-            
+            d_f_f_y.downloading_from_youtube(
+                str(url_txt), str(name_txt),\
+                     download_video_value.get(), \
+                        download_audio_value.get(), \
+                            authentication_value.get(), \
+                                authentication_once_value.get())
+    
     windows.title("Video download application")
-    windows.geometry("700x300+600+280")
+    windows.geometry("700x390+600+280")
+    windows.resizable(False, False)
+    windows.iconphoto(False, tk.PhotoImage(file='icon.png'))
 
     label_for_url = tk.Label(windows, 
                             text= "Enter URL for download video and audio Youtube", 
@@ -22,7 +41,7 @@ def start_graphical_interface():
     label_for_example_url = tk.Label(windows, 
                             text= "Example URL: https://www.youtube.com/watch?v=ХХХХХХХХХХХ", 
                             fg='black',
-                            font=('Arial', 8,'bold'))
+                            font=('Arial', 9,'bold'))
     
     label_for_name_download_file = tk.Label(windows, 
                             text= "Enter name for download file", 
@@ -36,15 +55,42 @@ def start_graphical_interface():
                             command=get_url_and_name_download_file)
 
     youtube_video_url = tk.Text(windows,
-                            width=100, 
-                            height=1, 
-                            font="Arial 8")
+                            width=60, 
+                            height=2, 
+                            font="Arial 14")
 
     name_for_download_file = tk.Text(windows,
                             width=45, 
                             height=1, 
-                            font="Arial 8")
+                            font="Arial 14")
+
+    flag_for_download_video = tk.Checkbutton(windows,
+                            text="Download video",
+                            font="Arial 16",
+                            variable=download_video_value,
+                            offvalue=0,
+                            onvalue=1)
+
+    flag_for_download_audio = tk.Checkbutton(windows,
+                            text="Download audio",
+                            font="Arial 16",
+                            variable=download_audio_value,
+                            offvalue=0,
+                            onvalue=1)
     
+    flag_for_authentication = tk.Checkbutton(windows,
+                            text="Perform authentication (log in to your account)",
+                            font="Arial 16",
+                            variable=authentication_value,
+                            offvalue=0,
+                            onvalue=1)
+    
+    flag_for_authentication_once = tk.Checkbutton(windows,
+                            text="Authentication (log in to your account) only once",
+                            font="Arial 16",
+                            variable=authentication_once_value,
+                            offvalue=0,
+                            onvalue=1)
     
     label_for_url.pack()
     label_for_url.place(x=115, y=0)
@@ -53,16 +99,27 @@ def start_graphical_interface():
     label_for_example_url.place(x=165, y=26)
 
     youtube_video_url.pack()
-    youtube_video_url.place(x=55, y=55)
+    youtube_video_url.place(x=16, y=55)
 
     label_for_name_download_file.pack()
-    label_for_name_download_file.place(x=187, y=90)
+    label_for_name_download_file.place(x=187, y=110)
 
     name_for_download_file.pack()
-    name_for_download_file.place(x=189, y=140)
+    name_for_download_file.place(x=95, y=145)
 
     start_button.pack()
-    start_button.place(x=245, y=190)
+    start_button.place(x=245, y=200)
 
-    windows.iconphoto(False, tk.PhotoImage(file='icon.png'))
+    flag_for_download_video.pack()
+    flag_for_download_video.place(x=110, y=270)
+
+    flag_for_download_audio.pack()
+    flag_for_download_audio.place(x=350, y=270)
+
+    flag_for_authentication.pack()
+    flag_for_authentication.place(x=100, y=305)
+
+    flag_for_authentication_once.pack()
+    flag_for_authentication_once.place(x=100, y=335)
+
     windows.mainloop()
